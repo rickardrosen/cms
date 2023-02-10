dd51d04e60d28cac926b83fe78ce121f6796ec34
Rickard Rosén <rickard.rosen@akindgroup.com>
2023-02-10 20:18:24 +0100

    style

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,29 +1,29 @@
 import { getTreeByName } from '$lib/server/github';
 
 interface TreeNode {
-  name: string;
-  type?: string;
-  path?: string;
-  children: TreeNode[];
+	name: string;
+	type?: string;
+	path?: string;
+	children: TreeNode[];
 }
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
-  const gitTree = await getTreeByName('docs');
-  const tree: TreeNode = {
-    name: 'Root',
-    children: []
-  };
-  tree.children = gitTree.reduce((r: TreeNode[], n) => {
-    n.path.split('/').reduce((childNodes: TreeNode[], name) => {
-      let child = childNodes.find((n) => n.name === name);
-      if (!child) {
-        childNodes.push((child = { name, path: n.path, children: [] }));
-      }
-      return child.children;
-    }, r);
-    return r;
-  }, []);
-  //console.log(tree);
-  return tree;
+	const gitTree = await getTreeByName('docs');
+	const tree: TreeNode = {
+		name: 'Root',
+		children: []
+	};
+	tree.children = gitTree.reduce((r: TreeNode[], n) => {
+		n.path.split('/').reduce((childNodes: TreeNode[], name) => {
+			let child = childNodes.find((n) => n.name === name);
+			if (!child) {
+				childNodes.push((child = { name, path: n.path, children: [] }));
+			}
+			return child.children;
+		}, r);
+		return r;
+	}, []);
+	console.log(tree);
+	return tree;
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,58 @@
 <script lang="ts">
   import Tree from './Tree.svelte'
-  /** @type {import('./$types').LayoutData} */
-  export let data
+  import type { LayoutData } from './$types'
+  export let data: LayoutData
 </script>
 
-<main>
-  <!-- +page.svelte is rendered in this <slot> -->
-  <slot />
-</main>
+<style>
+  .wrapper {
+    display: flex;
+    flex-flow: row wrap;
+  }
 
-<aside>
-  <h2>Files</h2>
-<Tree {data} />
-</aside>
+  /* We tell all items to be 100% width, via flex-basis */
+  .wrapper > * {
+    flex: 1 100%;
+  }
+
+  /* We rely on source order for mobile-first approach
+   * in this case:
+   * 1. header
+   * 2. article
+   * 3. aside 1
+   * 4. aside 2
+   * 5. footer
+   */
+
+  /* Medium screens */
+  @media all and (min-width: 600px) {
+    /* We tell both sidebars to share a row */
+    .aside { flex: 1 auto; }
+  }
+
+  /* Large screens */
+  @media all and (min-width: 800px) {
+    /* We invert order of first sidebar and main
+     * And tell the main element to take twice as much width as the other two sidebars 
+     */
+    .main { flex: 3 0px; }
+    .aside-1 { order: 1; }
+    .main    { order: 2; }
+    .aside-2 { order: 3; }
+    .footer  { order: 4; }
+  }
+</style>
+
+<div class="wrapper">
+  <header class="header">Header</header>
+  <main class="main">
+    <!-- +page.svelte is rendered in this <slot> -->
+    <slot />
+  </main>
+
+  <aside class="aside nav">
+    <Tree {data} />
+  </aside>
+  <aside class="aside aside-2">Aside 2</aside>
+  <footer class="footer">Footer</footer>
+</div>
