3f1f0cf625621a6b75d6036e3013ad1794ea668e
Rickard Rosén <rickard.rosen@akindgroup.com>
2023-02-13 16:36:45 +0100

    ipk

--- src/routes/view/[...slug]/+page.server.ts
+++ src/routes/view/[...slug]/+page.server.ts
@@ -4,13 +4,13 @@ import type { PageServerLoad } from './$types';
 import { marked } from 'marked';
 
 export const load = (async ({ params }) => {
-  const data = await getContent(params.slug);
-  if (!data) throw error(404);
-  if ('content' in data) {
-    const html = marked.parse(Buffer.from(data.content, 'base64').toString());
-    return { html };
-  }
-  return {
-    data
-  };
+	const data = await getContent(params.slug);
+	if (!data) throw error(404);
+	if ('content' in data) {
+		const html = marked.parse(Buffer.from(data.content, 'base64').toString());
+		return { md: Buffer.from(data.content, 'base64').toString() };
+	}
+	return {
+		data
+	};
 }) satisfies PageServerLoad;
--- src/routes/view/[...slug]/+page.svelte
+++ src/routes/view/[...slug]/+page.svelte
@@ -1,8 +1,64 @@
 <script lang="ts">
   import type { PageData } from './$types'
-  export let data
-
+  export let data: PageData
+	import { onDestroy, onMount } from 'svelte';
+	let showToolbar = true;
+	let textele;
+	let editor: EasyMDE;
+	// Setting up the editor and the visible buttons of the toolbar
+	onMount(async () => {
+		const easymde = await import('easymde');
+		const EasyMDE = easymde.default;
+		editor = new EasyMDE({
+			element: textele,
+			autofocus: true,
+			spellChecker: false,
+			nativeSpellcheck: true,
+			theme: 'easymde',
+			autosave: {
+				enabled: true,
+				uniqueId: 'easymde-autosave',
+				delay: 1000
+			},
+			toolbar: [
+				'preview',
+				'|',
+				'bold',
+				'italic',
+				'strikethrough',
+				'|',
+				'heading-bigger',
+				'heading-smaller',
+				'heading-1',
+				'heading-2',
+				'heading-3',
+				'|',
+				'unordered-list',
+				'ordered-list',
+				'|',
+				'code',
+				'image',
+				'link',
+				'quote',
+				'table',
+				'horizontal-rule',
+				'|',
+				'redo',
+				'undo',
+				'guide',
+				'|',
+			],
+			status: ['autosave', 'words'],
+			hideIcons: ['fullscreen', 'side-by-side'],
+		});
+	});
+	// Resetting the editor when clearing it
+	onDestroy(() => {
+		if (editor) {
+			editor.toTextArea();
+			editor = null
+		}
+	});
 </script>
 
-<h1>stuff</h1>
-<div>{@html data.html}</div>
+<div class="editor"><textarea id="invisible text area" bind:this={textele} /></div>
